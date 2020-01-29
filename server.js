66e89438facbd0d8ca1cc2650261514997e5b47b
Jonathan <jonretchless@gmail.com>
2020-01-28 19:21:33 -0800

    init

---
+++ server.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const movies = require('./movies.json')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})
+
+app.get('/movie', function handleGetMovies(req, res) {
+  let response = movies;
+
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    )
+  }
+
+  // filter our pokemon by type if type query param is present
+    if (req.query.country) {
+        response = response.filter(movies =>
+        movies.country.includes(req.query.country)
+        )
+    }
+
+    if (req.query.avg_vote) {
+        response = response.filter(movies =>
+            movies.avg_vote >=req.query.avg_vote
+        ) 
+    }
+  res.json(response)
+})
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
